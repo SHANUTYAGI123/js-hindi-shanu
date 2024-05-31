cbd209102096ea54875560221339c969f68c92fa
SHANUTYAGI123 <143880718+SHANUTYAGI123@users.noreply.github.com>
2024-05-31 17:51:14 +0000

    dataypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Shanu")
+
+//Datatypes 
+
+let name="shanu" //string datatypes
+let age=13 //integer datatypes
+let isLoggedIn=false //boolean datatypes
+
+/*
+number=> 2 to power 53
+bigint
+string =>""
+boolean=> true/false
+null => standalone value
+undefined =>  value is not defined right now
+symbol => unique
+*/
+console.log(typeof(name)); //string 
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
