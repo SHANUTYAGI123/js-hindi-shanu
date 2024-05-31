582abcb16e0c31ac241e76136be26836516507ba
SHANUTYAGI123 <143880718+SHANUTYAGI123@users.noreply.github.com>
2024-05-31 18:26:07 +0000

    datatypes conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score="33abc"
+
+console.log(typeof score);
+console.log((typeof score));
+
+let value =Number(score)
+console.log(value)
+
+console.log(typeof value);
+console.log((typeof value));
+
+/* 
+"33"=>33
+"33abc"=>NaN
+true=>1; false=>0
+*/
+
+let isLoggedIn=1
+
+let booleanIsLoogedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn)
+
+console.log(typeof booleanIsLoogedIn);
+
+let someNumber =33
+
+let stringNumber=String(someNumber)
+console.log(someNumber)
+console.log(typeof someNumber)
