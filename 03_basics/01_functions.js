5651a914ec214fa7e049ac308c4f94a60b937bab
SHANUTYAGI123 <143880718+SHANUTYAGI123@users.noreply.github.com>
2024-06-14 18:54:16 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,4 +34,37 @@ function logInUserMessage(username="You"){
     return `${username} just logged in`
 }
 
-console.log(logInUserMessage());
+// console.log(logInUserMessage());
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(100,200,556));
+
+const user={
+    username: "Shanu",
+    price:299
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// function handleObject(user){
+//     console.log(`username is ${user.username} and price is ${user.price}`);
+// }
+
+// handleObject(user)
+handleObject({
+    username:"Shanu",
+    price:299
+})
+
+const myNewArray=[200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,100,600]));
+
